264af2a21d0c5294dfb216d5c2961ccf7ba2dc88
Douglas Faria <hfsdouglas@gmail.com>
2023-07-03 22:09:25 -0300

    feat: tirar foto, tentar novamente e botão de envio

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -170,8 +170,26 @@ export default class WhatsAppController {
         });
 
         this.el.btnTakePicture.on('click', (event) => {
-            console.log('TakePicture');
+            let dataUrl = this._camera.takePicture();
+            this.el.pictureCamera.src = dataUrl;
+            this.el.pictureCamera.show();
+            this.el.videoCamera.hide();
+            this.el.btnReshootPanelCamera.show();
+            this.el.containerTakePicture.hide();
+            this.el.containerSendPicture.show();   
         });
+
+        this.el.btnReshootPanelCamera.on('click', (event) => {
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide()
+        }); 
+
+        this.el.btnSendPicture.on('click', (event) => {
+            console.log(this.this.el.pictureCamera.src);
+        })
 
         this.el.btnAttachDocument.on('click', (event) => {
             this.closeAllMainPanel();
